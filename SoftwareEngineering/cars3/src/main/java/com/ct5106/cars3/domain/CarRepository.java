bcdfdb5e55f60742bcbe12c71bf75be35b1878d4
Oisin Mc Laughlin <omclaughlin99@gmail.com>
2024-09-28 16:08:28 +0100

    Minor changes

--- SoftwareEngineering/cars3/src/main/java/com/ct5106/cars3/domain/CarRepository.java
+++ SoftwareEngineering/cars3/src/main/java/com/ct5106/cars3/domain/CarRepository.java
@@ -12,20 +12,20 @@ public interface CarRepository extends CrudRepository<Car, Long>
 {
 
 	List<Car> findByMake (String make);
-	 
+
 	List<Car> findByYearRegistered (int year);
-	
+
 	List<Car> findByMakeAndModel(String make, String model);
 
 	List<Car> findByMakeOrModel(String make, String model);
-	
+
 	List<Car> findByMakeOrderByPriceDesc(String make);
-	
+
 	// Fetch cars by price range using SQL
 	@Query("select c from Car c where c.price between ?1 and ?2")
 	List<Car> findByPriceBetween(@Param("low") double low, @Param("high") double high);
-	
+
 	// Fetch cars with reg containing county string, e.g. "KY"
 	List<Car> findByRegistrationContaining(@Param("county") String county);
-	
+
 }
