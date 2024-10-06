16c36fa7d0cf765412ff32c45f2e599365841d6b
Oisin Mc Laughlin <omclaughlin99@gmail.com>
2024-10-06 20:25:12 +0100

    Asteroids spawn from edges and move in random directions with screen wrapping

--- GameDevelopment/Asteroids/Assets/Asteroid.cs
+++ GameDevelopment/Asteroids/Assets/Asteroid.cs
@@ -4,15 +4,62 @@ using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
+    private Vector3 screenBounds;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
+        // Generate random direction for asteroid
+        Vector3 randDir = new Vector3(Random.Range(-1f, 1f), 0, Random.Range(-1f, 1f));
 
+        // Normalize so asteroid moves constant direction
+        randDir.Normalize();
+        
+        // Randomize asteroid velocity
+        float randVel = Random.Range(1f, 3f);
+        
+        // Set asteroid velocity
+        GetComponent<Rigidbody>().AddForce(randDir * randVel, ForceMode.Impulse);
+        
+        // Calculate screenbounds from where camera is looking
+        screenBounds = Camera.main.ViewportToWorldPoint(new Vector3(1, 1, Camera.main.transform.position.y));
+    }
+    
+    private float timeAccumulator = 0;
+    
     // Update is called once per frame
     void Update()
     {
+        // Add time passed since last frame
+        timeAccumulator += Time.deltaTime;
         
+        // If 0.2 seconds have passed, call CheckBounds (in theory this is 5 times a second)
+        if (timeAccumulator >= 0.2f) {
+            CheckBounds();
+            timeAccumulator = 0;
+        }
+    }
+
+    void CheckBounds()
+    {
+        Vector3 pos = transform.position;
+        
+        // Wrap asteroid around screen if it goes out of bounds
+        if (pos.x > screenBounds.x) {
+            pos.x = -screenBounds.x;
+        }
+        else if (pos.x < -screenBounds.x) {
+            pos.x = screenBounds.x;
+        }
+        
+        if (pos.z > screenBounds.z) {
+            pos.z = -screenBounds.z;
+        }
+        else if (pos.z < -screenBounds.z) {
+            pos.z = screenBounds.z;
+        }
+        
+        // Set new position of the wrapped asteroid
+        transform.position = pos;
     }
 }
--- GameDevelopment/Asteroids/Assets/GameManager.cs
+++ GameDevelopment/Asteroids/Assets/GameManager.cs
@@ -6,6 +6,7 @@ public class GameManager : MonoBehaviour
 {
     private int currentGameLevel;
     public GameObject asteroid;
+    // public int totalAsteroids = 0;
     
     // Start is called before the first frame update
     void Start()
@@ -40,10 +41,35 @@ public class GameManager : MonoBehaviour
         for (int i = 0; i < currentGameLevel * 2; i++)
         {
             // Randomize asteroid spawn position
-            Vector3 spawnPos = new Vector3(Random.Range(-15, 15), 0, Random.Range(-15, 15));
+            // Vector3 spawnPos = new Vector3(Random.Range(-15, 15), 0, Random.Range(-15, 15));
+            Vector3 spawnPos = Vector3.zero;
+            int edge = Random.Range(0, 4);
+
+            // Bottom edge
+            if (edge == 0) {
+                // Randomize x between left and right edges and set z to bottom edge
+                spawnPos = new Vector3(Random.Range(bottomLeft.x, topRight.x), 0, bottomLeft.z);
+            }
+            // Top edge
+            else if (edge == 1) {
+                // Randomize x between left and right edges and set z to top edge
+                spawnPos = new Vector3(Random.Range(bottomLeft.x, topRight.x), 0, topRight.z);
+            }
+            // Left edge
+            else if (edge == 2) {
+                // Randomize z between bottom and top edges and set x to left edge
+                spawnPos = new Vector3(bottomLeft.x, 0, Random.Range(bottomLeft.z, topRight.z));
+            }
+            // Right edge
+            else if (edge == 3) {
+                // Randomize z between bottom and top edges and set x to right edge
+                spawnPos = new Vector3(topRight.x, 0, Random.Range(bottomLeft.z, topRight.z));
+            }
             
             // Instantiate asteroid at spawn position with no rotation
             Instantiate(asteroid, spawnPos, Quaternion.identity);
+            // totalAsteroids++;
+            // Debug.Log("Asteroid Spawned!\nToal Asteroids: " + totalAsteroids);
         }
     }
 }
