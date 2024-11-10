b73bce46bd0b8ec959ce88b3fd30af86c508d2cd
Oisin Mc Laughlin <omclaughlin99@gmail.com>
2024-11-10 14:54:40 +0000

    Question 1

--- ProgrammingParadigms/Assignment2Lisp/assignment2_q1.rkt
+++ ProgrammingParadigms/Assignment2Lisp/assignment2_q1.rkt
@@ -1,4 +1,70 @@
 #lang racket
 
 ; Question 1
-(display "Hello World!")
+; Oisin Mc Laughlin - 22441106
+
+; Part A
+
+; 1
+(display "A cons pair of two numbers: ")
+
+(cons 8 9)
+
+(display "\n")
+
+
+; 2
+(display "A list of 3 numbers, using only the cons function: ")
+
+; For cons 3 the pair number needs to be a null value '()
+(cons 1 (
+         cons 2 (
+                 cons 3 '()
+                      )
+              )
+      )
+
+(display "\n")
+
+; 3
+(display "A list containing a string, a number and a nested list of three numbers, using only the cons function: ")
+
+(cons "Hello World!"
+      (cons 99
+            ; Put nested list in another cons
+            (cons (cons 6
+                        (cons 5
+                              (cons 4 '()
+                                    )
+                              )
+                        )
+                  '()
+                  )
+            )
+      )
+
+
+(display "\n")
+
+
+; 4
+(display "A list containing a string, a number and a nested list of three numbers, using only the list function: ")
+
+(list
+ "World Hello!"
+      12
+       (list 9 8 7)
+      )
+
+(display "\n")
+
+
+; 5
+(display "A list containing a string, a number and a nested list of three numbers, using only the append function: ")
+
+; First I will define 3 lists for the string, number and list of 3 numbers.
+(define str '("!dlroW olleH"))
+(define num '(420))
+(define numL '(4 5 6))
+
+(append str num numL)
---
+++ ProgrammingParadigms/Assignment2Lisp/assignment_q2.rkt
@@ -0,0 +1,17 @@
+#lang racket
+
+;This is an example implementation of ins_beg,
+;It obviously doesn't do what it should, so you
+;can edit this function to get started.
+;
+;Please note the provide function is necessary
+;for the unit tests to work. Please include a
+;(provide) for each function you write in your
+;submitted assignment.
+;
+;You may delete these comments!
+
+(provide ins_beg)
+
+(define (ins_beg el lst)
+  (display "Hello, I'm ins_beg!\n"))
