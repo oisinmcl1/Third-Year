014278ab8a422d79f1b878b3807db0937e14664c
Oisin Mc Laughlin <omclaughlin99@gmail.com>
2024-11-30 15:55:59 +0000

    question 1

--- ProgrammingParadigms/Assignment3Prolog/question1.pl
+++ ProgrammingParadigms/Assignment3Prolog/question1.pl
@@ -1,3 +1,6 @@
+% Assignment 3 - Q1
+% Oisin Mc Laughlin - 22441106
+
 takes(tom, ct331).
 takes(mary, ct331).
 takes(joe, ct331).
@@ -5,3 +8,9 @@ takes(tom, ct345).
 takes(mary, ct345).
 instructs(bob, ct331).
 instructs(ann, ct345).
+
+% Write a prolog rule called ‘teaches’ that returns true if a given instructor teaches a given student
+teaches(X, Y) :- instructs(X, Z), takes(Y, Z).
+
+% Write a prolog rule called ‘classmates’ that returns true if two students take the same course.
+classmates(X,Y) :- takes(X, Z), takes(Y, Z).
